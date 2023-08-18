524fa037609e338b81f4338029912dc0dce58b95
Klajdi2004 <klajdi.dhana1@gmail.com>
2023-08-18 08:45:13 -0700

    test

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-''' flask setup'''
-from flask import Flask
-from flask import render_template
+# Script that starts a Flask web application:
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def hello_route():
+    """ return Hello HBNB! """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
+@app.route('/hbnb')
+def hbnb_route():
+    """ return HBNB """
     return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def str_txt(text):
-    formatted_text = text.replace('_', ' ')
-    result = f"C {formatted_text}"
-    return result
+@app.route('/c/<text>')
+def c_route(text):
+    """ return c followed by the value """
+    return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python/')
-@app.route('/python/<text>', strict_slashes=False)
-def pyt_txt(text='is cool'):
-    for_txt = text.replace('_', ' ')
-    result = f"Python {for_txt}"
-    return result
+@app.route('/python/<text>')
+def python_route(text="is cool"):
+    """ return followed by the value of the text variable """
+    return "Python {}".format(text.replace("_", " "))
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def num_txt(n):
-    return '{} is a number'.format(n)
+@app.route('/number/<int:n>')
+def number_route(n):
+    """display n is a number only if n is integer"""
+    return "{} is a number".format(n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def num_temp(n=None):
-    return render_template('5-number.html', n=n)
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """display a HTML page only if n is integer
+    H1 tag: Number: n inside the tag BODY
+    """
+    return render_template('5-number.html', number=n)
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
